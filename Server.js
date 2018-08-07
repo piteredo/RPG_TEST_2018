39d63db56552f2d4f266f650eec465b26d818c46
piteredo <piteredo@hotmail.com>
2018-08-08 04:17:32 +0900

    add npc(test

--- Server.js
+++ Server.js
@@ -34,7 +34,45 @@ let io = require("socket.io").listen(server);
 
 
 let playerList = {};
+let timer;
 io.sockets.on("connection", function(socket){
+
+  //ちゃんとする
+  let count = 1;
+  function npcWalk(){
+    let dirArr = [
+      {dir:"left", x:-1, y:1},
+      {dir:"up", x:-1, y:-1},
+      {dir:"right", x:1, y:-1},
+      {dir:"down", x:1, y:1}
+    ];
+    let id = "degsfhxzfght";
+    playerList[id].isAnimating = true;
+    playerList[id].dir = dirArr[count%4].dir;
+    socket.broadcast.emit("updateIsAnimating", playerList[id]);
+    playerList[id].isWalking = true;
+    //playerList[id].dir = dir;
+    playerList[id].nextX = playerList[id].x + dirArr[count%4].x;
+    playerList[id].nextY = playerList[id].y + dirArr[count%4].y;
+    //playerList[id].x++;
+    //playerList[id].y++;
+    socket.broadcast.emit("updateIsWalking", playerList[id]);
+    count++;
+  }
+  if(Object.keys(playerList).length == 0){
+    console.log("create npc");
+    let npcData = {
+      id: "degsfhxzfght",
+      x: 8,
+      y: 4,
+      dir: "down",
+      isAnimating: false,
+      isWalking: false
+    };
+    playerList["degsfhxzfght"] = npcData;
+    timer = setInterval(npcWalk, 1000);
+  }
+
 
   socket.on("newConnection", function(x, y, dir, isAnimating, isWalking){
     let id = socket.id;
@@ -46,6 +84,7 @@ io.sockets.on("connection", function(socket){
       isAnimating: isAnimating,
       isWalking: isWalking
     };
+
     io.to(id).emit("newConTo", id, playerList);
     playerList[id] = data;
     socket.broadcast.emit("newConBroadcast", data);
